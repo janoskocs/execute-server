5c8b490a2f722394c0f0085e61d0a9524a874395
Janos Kocs <janos.kocs@outlook.com>
2023-12-26 20:16:37 +0000

    chore: set up app to and env variables

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const app = express();
+require('dotenv').config();
+const app = require('./app.js');
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
 });
