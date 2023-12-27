f5ce334b8dab871ef60c9566c39f242bb5eb602b
Janos Kocs <janos.kocs@outlook.com>
2023-12-27 11:58:33 +0000

    feature: implement server and DB connection

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+const express = require('express');
 const app = require('./app.js');
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({alive: 'True'});
 });
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+      app.listen(port, () => {
+        console.log(`Connected to DB and listening on port ${port}...`);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+
